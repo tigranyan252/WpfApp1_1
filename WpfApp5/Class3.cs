5fa3ff825eea2a5e8c2961cc0ad1baada6734690
Serj Tigranyan <serj-tigranyan@mail.ru>
2022-06-28 16:57:09 +0200

    Добавьте файлы проекта.

---
+++ WpfApp5/Class3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp5
+{
+    internal class Class3
+    {
+        private double v1;
+        private double v2;
+
+        public Class3(double v1, double v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        internal object Result()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
